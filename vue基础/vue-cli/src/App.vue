3876651693c874e7adbce7afb9c96ae8099cf040
yxz123456 <15826855545 @163.com>
2019-01-15 20:59:30 +0800

    vue-cli

---
+++ vue基础/vue-cli/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <my-component></my-component>
+  </div>
+</template>
+
+<script>
+
+import MyComponent from "./components/myComponent";
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent,
+
+  }
+}
+</script>
+
+<!--入口文件css权重高于组件-->
+<style>
+
+</style>
---
+++ vue基础/vue-cli/src/components/myComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="wrapper">
+        <div>我是{{name}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "myComponent",
+        data(){
+          return {
+            name:"myComponent"
+          }
+        }
+    }
+</script>
+
+<style scoped>
+    .wrapper{
+      width: 100px;
+      height: 100px;
+      background-color: #007DDC;
+    }
+</style>
